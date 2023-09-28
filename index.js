27292e8e8998d1698bab4b78a9e2443e2f23efb3
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-09-28 14:00:18 +0300

    task done

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(a, b){
+    return a/b;
+}
+
+const square= (x) => {
+    let result=1;
+    for(let i=0;i<x;i++)
+        result*=x;
+    return result;
+}
+
+const add = (a,b) => a+b;
